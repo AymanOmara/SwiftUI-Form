202d59c1e1d682cd67363b93292bc2fc4c3b53c0
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-04-17 10:59:31 +0200

    init commit

---
+++ SwiftUIForm/SwiftUIFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormApp.swift
+//  SwiftUIForm
+//
+//  Created by Ayman Omara on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
